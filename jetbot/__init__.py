45a7e6dacd89ce71362fd7601d89904077bb8dce
glendaleonard <glendaleonard@gmail.com>
2020-02-23 20:42:47 -0500

    added a_star.py

--- jetbot/__init__.py
+++ jetbot/__init__.py
@@ -4,3 +4,5 @@ from .motor import Motor
 from .robot import Robot
 from .image import bgr8_to_jpeg
 from .object_detection import ObjectDetector
+from .a_star import AStar
+print('running __init__.py')
